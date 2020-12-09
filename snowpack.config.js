ad40ab2012a3724862ea720bbcd8e18be3300b33
Benjamin Seber <benjamin.seber@mailbox.org>
2020-12-09 22:36:59 +0100

    bump snowpack-plugin-cache-bust and enable it again
    plugin had a bug which resulted in closing the write stream to fast
    (actually the wrong stream has been used to signal a finished html transformation)

--- snowpack.config.js
+++ snowpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     "@snowpack/plugin-postcss",
     ["@snowpack/plugin-run-script", { cmd: "eleventy", watch: "$1 --watch" }],
     ["@snowpack/plugin-run-script", { cmd: copyAlpineJs }],
-    // "snowpack-plugin-cache-bust",
+    "snowpack-plugin-cache-bust",
   ],
   devOptions: {
     port: 3000,
